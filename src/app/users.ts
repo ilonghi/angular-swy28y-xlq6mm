d0cce8e14c6d782c89f94c4a6e1516b92b5b673f
ilonghi <ivan.longhi@gmail.com>
2020-03-04 13:58:56 +0100

    1 file added

---
+++ src/app/users.ts
@@ -0,0 +1,30 @@
+export const users = [
+  {
+    "username": "JOHN",
+    "firstName": "John",
+    "lastName": "Doe",
+    "fullName": "Doe John",
+    "email": "j.doe@example.com",
+    "mobilePhone": null,
+    "groups": [
+      "ADMIN",
+      "PM"
+    ],
+    "serviceUser" : false,
+    "disabled"	: false
+  },
+  {
+    "username": "ROBY",
+    "firstName": "Roby",
+    "lastName": "Facks",
+    "fullName": "Facks Roby",
+    "email": "r.facks@example.com",
+    "mobilePhone": "3599966542",
+    "groups": [
+      "ADMIN",
+      "PM"
+    ],
+    "serviceUser" : false,
+    "disabled"	: false
+  }
+];
